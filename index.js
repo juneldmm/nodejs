f4097bf5304256d7528e76c5f0f0e92249a35836
maggie <619580368@qq.com>
2016-10-07 14:49:36 +0800

    'tow'

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-/**
- * Created by Administrator on 2016/9/27.
- */
+
 var app = require('express')();//引入express模块，并运行
-var http = require('http').Server();//引入http模块，用于http请求
+var http = require('http').Server(app);//引入http模块，用于http请求
+
+var io = require('socket.io')(http)//引入socket模块，同时将http传进去，用它的方法
 
 app.get('/',function(request,response){
 
-  response.send('hello word');
+  response.sendFile(__dirname+'/index.html');
 
 });
 
-  http.listen(3344,function(){
+io.on('connection',function(socket){
+    socket.on('chat.message',function(message){
+        //console.log('socket.io'+message);//message是用户输入的信息
+        io.emit('chat.message',message);//通过emit方法，将信息广播给所有用户
+
+    })
+
+})
+
+  http.listen(3222,function(){
       console.log('serve start');
   });
+
 
 
 
